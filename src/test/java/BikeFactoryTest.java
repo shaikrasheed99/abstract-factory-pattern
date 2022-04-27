393f983d9598ffbe2c38bbe08d56285841991da2
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-27 22:47:24 +0530

    Refactor introduce before each test method to set up bike factory

--- src/test/java/BikeFactoryTest.java
+++ src/test/java/BikeFactoryTest.java
@@ -1,12 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BikeFactoryTest {
+    private BikeFactory bikeFactory;
+
+    @BeforeEach
+    void setUpBikeFactory() {
+        bikeFactory = new BikeFactory();
+    }
+
     @Test
     void shouldBeAbleToCreateHeroBike() {
-        BikeFactory bikeFactory = new BikeFactory();
-
         Vehicle hero = bikeFactory.createVehicleWithType(BIKE_TYPE.HERO);
 
         assertTrue(hero instanceof Hero);

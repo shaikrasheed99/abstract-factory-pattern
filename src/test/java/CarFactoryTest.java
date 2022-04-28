964cb612865c0fe2f536651bc77d9e390ac71196
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-28 10:21:35 +0530

    Refactor introduce before each test method to set up car factory

--- src/test/java/CarFactoryTest.java
+++ src/test/java/CarFactoryTest.java
@@ -1,13 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CarFactoryTest {
+    private CarFactory carFactory;
+
+    @BeforeEach
+    void setUpCarFactory() {
+        carFactory = new CarFactory();
+    }
+
     @Test
     void shouldBeAbleToCreateTATACar() {
-        CarFactory carFactory = new CarFactory();
-
         Vehicle tata = carFactory.createVehicleWithType(CAR_TYPE.TATA);
 
         assertTrue(tata instanceof Tata);
@@ -15,8 +21,6 @@ public class CarFactoryTest {
 
     @Test
     void shouldBeAbleToCreateSUZUKICar() {
-        CarFactory carFactory = new CarFactory();
-
         Vehicle suzuki = carFactory.createVehicleWithType(CAR_TYPE.SUZUKI);
 
         assertTrue(suzuki instanceof Suzuki);
@@ -24,8 +28,6 @@ public class CarFactoryTest {
 
     @Test
     void shouldNotBeAbleToCreateAnyVehicleWithNoInput() {
-        CarFactory carFactory = new CarFactory();
-
         assertNull(carFactory.createVehicleWithType(null));
     }
 }
